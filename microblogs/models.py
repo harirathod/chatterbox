d112f428ed6e89c315d6faa64219a0f7ef338444
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-08-04 14:53:14 +0100

    Add tests for User and a bio and username field

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import django.contrib.auth.validators
 
 # Create your models here.
 
 class User(AbstractUser):
-    pass
+    """Class defining a user for the microblogs app."""
+    username = models.CharField(error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=30, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')
+    bio = models.TextField(default="No bio provided.")
--- microblogs/tests.py
+++ microblogs/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from .models import User
+from django.core.exceptions import ValidationError
+# Unit tests go here.
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+    def setUp(self):
+        self.user =  User.objects.create_user(
+            '@personOne',
+            first_name='person',
+            last_name='one',
+            email='personOne@example.org',
+            password='PasswordPersonOne',
+            bio="Person One's Bio."
+        )
+
+    def test_valid_user(self):
+        self._assert_user_is_valid(self.user)
+
+    def test_username_not_blank(self):
+        self.user.username = ''
+        self._assert_user_is_invalid(self.user)
+
+    def test_password_not_blank(self):
+        self.user.password = ""
+        self._assert_user_is_invalid(self.user)
+
+    def test_username_can_be_30_chars(self):
+        """We don't want usernames to have a length longer than 30 characters, as there are not very recognisable / rememberable.."""
+        self.user.username = "a" * 30
+        self._assert_user_is_valid(self.user)
+
+    def test_username_cannot_be_over_30_chars(self):
+        """We don't want usernames to have a length longer than 30 characters, as there are not very recognisable / rememberable.."""
+        self.user.username = "a" * 31
+        self._assert_user_is_invalid(self.user)
+
+    def _assert_user_is_valid(self, user: User):
+        try:
+            user.full_clean()
+        except ValidationError as e:
+            self.fail("The user used in the 'test_valid_user' test case is supposed to be valid. This error message indicates that the user was invalid.")
+
+    def _assert_user_is_invalid(self, user: User):
+        with self.assertRaises(ValidationError):
+            user.full_clean()
