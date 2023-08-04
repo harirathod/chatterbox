c862f62b1ada64c4aad64f444750b1ed3ac95195
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-08-04 14:51:53 +0100

    Add class for admin user

--- microblogs/admin.py
+++ microblogs/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+"""Configuration of the admin in the microblogs app."""
 
+from django.contrib import admin
+from .models import User    # .models is used to show we are referring to the current directory. models doesn't work.
 # Register your models here.
+
+@admin.register(User)
+class AdminUser(admin.ModelAdmin):
+    """Class defining an admin user, in the microblogs app."""
+    list_display = [
+        'username', 'first_name', 'is_active', 'date_joined', 'email',
+    ]
