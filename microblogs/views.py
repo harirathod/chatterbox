aa28ab15a8a1cdb2e069b3cff95eaa88d447cd2f
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-08-04 14:52:29 +0100

    Add docstring for home method

--- microblogs/views.py
+++ microblogs/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    """Returns the 'home.html' page when the request is made to this function."""
     # response = HttpResponse("Welcome to this blogging application!")
     return render(request, "home.html")
