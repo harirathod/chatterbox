c963be44300a641d06dc3d8cbf40a996fd94d3ff
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-10-06 17:23:40 +0100

    Create seed and unseed command templates

---
+++ microblogs/management/commands/seed.py
@@ -0,0 +1,5 @@
+from django.core.management.base import CommandError, BaseCommand
+from faker import Faker
+
+class SeedCommand(BaseCommand):
+    pass
---
+++ microblogs/management/commands/unseed.py
@@ -0,0 +1,5 @@
+from django.core.management.base import BaseCommand, CommandError
+from faker import Faker
+
+class UnseedCommand(BaseCommand):
+    pass
