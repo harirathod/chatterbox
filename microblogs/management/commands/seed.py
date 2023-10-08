94290169b352e721e2ca38c577bda9f36aa1c1d0
Hari Rathod <hari.rathod@kcl.ac.uk>
2023-10-08 14:48:50 +0100

    Implement seed and unseed commands

--- microblogs/management/commands/seed.py
+++ microblogs/management/commands/seed.py
@@ -1,5 +1,29 @@
 from django.core.management.base import CommandError, BaseCommand
 from faker import Faker
+from microblogs.models import User
 
-class SeedCommand(BaseCommand):
-    pass
+# This class must be named Command to be recognised by Django. We cannot give it a custom name.
+
+class Command(BaseCommand):
+    """This class defines what happens when the 'seed' command is called.
+    
+    It creates 100 fake entries in the database."""
+    def __init__(self):
+        self.faker = Faker('en_GB')
+
+    def handle(self, *args, **options):
+        """Create 100 fake entries in the database."""
+        for _ in range(100):
+            # If we just used 'create', this wouldn't hash the password. Thus, we need to user "create_user()"
+            user = User.objects.create_user(
+                username = self.faker.unique.user_name(),
+                password = self.faker.password(),
+                bio = self.faker.text(),
+                first_name = self.faker.first_name(),
+                last_name = self.faker.last_name(),
+                email = self.faker.email(),
+            )
+            # user.save() is not needed. 
+        print("Created fake users.")
+
+# Currently on the new branch
--- microblogs/management/commands/unseed.py
+++ microblogs/management/commands/unseed.py
@@ -1,5 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
-from faker import Faker
+from microblogs.models import User
 
-class UnseedCommand(BaseCommand):
-    pass
+# This class must be named Command to be recognised by Django. We cannot give it a custom name.
+
+class Command(BaseCommand):
+    """This class defines what happens when the 'unseed' command is called. 
+    
+    It deletes all entries from the database."""
+    def __init__(self):
+        super().__init__()
+        
+    def handle(self, *args, **options):
+        """Delete all users from the database."""
+        for user in User.objects.all():
+            user.delete()
+        print("Deleted all users from the database.")
+    
+# Currently on the new branch
